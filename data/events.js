8d962ad59454f216d3b4cd5d40946aadbd22a6c4
Marzanipour <90430587+Marzanipour@users.noreply.github.com>
2026-09-14 23:02:28 -0400

    Publish September medical school fair events

--- data/events.js
+++ data/events.js
@@ -1,1 +1,24 @@
-window.AMSA_EVENTS = [];
+window.AMSA_EVENTS = [
+  {
+    title: "Medical School Fair at NYU",
+    type: "Medical School Fair",
+    date: "September 22, 2026",
+    time: "10:00 AM - 3:00 PM",
+    location: "NYU Silver Center, Hemmerdinger Hall",
+    description:
+      "Meet representatives from U.S., Caribbean, Mexican, and Australian medical programs, plus pharmacy and MCAT resources.",
+    tags: ["Pre-health", "Medical schools", "International"],
+    link: "https://imsfevent.com/event/new-york-medical-school-fair-2026/"
+  },
+  {
+    title: "Clinical Skills Workshop: Suturing",
+    type: "Workshop",
+    date: "September 22, 2026",
+    time: "4:00 PM - 6:00 PM",
+    location: "NYU Silver Center, Hemmerdinger Hall",
+    description:
+      "A hands-on suturing workshop limited to 60 students. Participants receive a certificate of completion.",
+    tags: ["Clinical skills", "Suturing", "Certificate"],
+    link: "https://docs.google.com/forms/d/e/1FAIpQLSc0EMfBfESJLh-QuNZ_GT8D6NcP8hfcFa44K3g5JPt7N7sUXQ/viewform?usp=header"
+  }
+];
